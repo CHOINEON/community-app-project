c5a2565d3d5e7209b04e7160a46aede3cda23489
KSH <tjdgu3408@naver.com>
2022-06-25 12:37:22 +0000

    separtate natural module

---
+++ mecabtest.js
@@ -0,0 +1,13 @@
+let mecab = require('mecab-ya');
+let text = '아버지가방에들어가신다.';
+
+console.log(text);
+mecab.pos(text, (err, result) => {
+    console.log('pos : ', result);
+});
+
+mecab.morphs(text, (err, result) => {
+    console.log('morphs : ', result);
+});
+
+console.log('nouns : ', mecab.nounsSync(text));
