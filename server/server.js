969a957dfbc65587552edcf90c235d0c0130ae57
kangsunghyun111 <tjdgu3408@naver.com>
2022-10-06 23:28:20 +0900

    cookie

--- server/server.js
+++ server/server.js
@@ -9,8 +9,9 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
 app.use(cookieParser());
+app.use(cors());
+
 app.use('/', router);
 
 
